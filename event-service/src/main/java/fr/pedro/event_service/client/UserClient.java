22cb8cfdfa8c902e5023d499ce5e504e9a559fc3
Pierre POCHERON <pierre.pocheron@gmail.com>
2025-07-07 15:11:57 +0200

    fix(client user): error on client user (fk event to user)

--- event-service/src/main/java/fr/pedro/event_service/client/UserClient.java
+++ event-service/src/main/java/fr/pedro/event_service/client/UserClient.java
@@ -1,5 +1,6 @@
 package fr.pedro.event_service.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -8,14 +9,17 @@ import org.springframework.web.client.RestTemplate;
 public class UserClient {
 
     private final RestTemplate restTemplate;
+    private final String userServiceBaseUrl;
 
-    public UserClient() {
+    public UserClient(@Value("${user-service.base-url:http://localhost:8081}") String userServiceBaseUrl) {
+        this.userServiceBaseUrl = userServiceBaseUrl;
         this.restTemplate = new RestTemplate();
     }
 
     public boolean checkUserExists(String userId) {
         try {
-            ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/api/users/" + userId, String.class);
+            String url = String.format("%s/api/users/%s", userServiceBaseUrl, userId);
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
             return response.getStatusCode().is2xxSuccessful();
         } catch (Exception e) {
             return false;
